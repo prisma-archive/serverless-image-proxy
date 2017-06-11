2c7edfba7dcf555084b94540cadc170caf37a934
Johannes Schickling <schickling.j@gmail.com>
2017-06-11 12:03:47 +0200

    Close #4

--- src/parser.ts
+++ src/parser.ts
@@ -23,12 +23,16 @@ interface Params {
   crop: string | undefined
 }
 
+const resizePattern = /^(\d*)x(\d*)(!?)$/
+const cropPattern = /^(\d*)x(\d*):(\d*)x(\d*)$/
+
 // valid paths:
 // /v1/ciwkuhq2s0dbf0131rcb3isiq/cj37jinmt008o0108zwhax711
 // /v1/ciwkuhq2s0dbf0131rcb3isiq/cj37jinmt008o0108zwhax711/600x200
 // /v1/ciwkuhq2s0dbf0131rcb3isiq/cj37jinmt008o0108zwhax711/600x200/20x20
 export function parseParams(path: string): Params {
-  const [,,...parts] = path.split('/')
+  // also trim trailing slash
+  const [,,...parts] = path.replace(/\/$/, '').split('/')
 
   if (parts.length < 2) {
     throw new Error(`Invalid path: ${path}`)
@@ -39,11 +43,24 @@ export function parseParams(path: string): Params {
   let resize: (string | undefined) = undefined
   let crop: (string | undefined) = undefined
 
-  if (parts.length === 3) {
-    resize = parts[2]
-  } else if (parts.length === 4) {
-    crop = parts[2]
-    resize = parts[3]
+  if (parts.length >= 3) {
+    if (parts[2].match(resizePattern)) {
+      resize = parts[2]
+    } else if (parts[2].match(cropPattern)) {
+      crop = parts[2]
+    } else {
+      throw new Error(`Invalid resize or crop pattern: ${parts[2]}`)
+    }
+  }
+
+  if (parts.length >= 4) {
+    if (parts[3].match(resizePattern)) {
+      resize = parts[3]
+    } else if (parts[3].match(cropPattern)) {
+      crop = parts[3]
+    } else {
+      throw new Error(`Invalid resize or crop pattern: ${parts[3]}`)
+    }
   }
 
   return { projectId, fileSecret, resize, crop }
@@ -62,12 +79,7 @@ export function getConfig(props: ConfigProps): ThumborConfig {
 }
 
 function extractResize(str: string): Resize {
-  const matches = str.match(/^(\d*)x(\d*)(!?)$/)!
-  if (!matches) {
-    throw new Error(`Invalid resize string: ${str}`)
-  }
-
-  const [, widthStr, heightStr, forceStr] = matches
+  const [, widthStr, heightStr, forceStr] = str.match(resizePattern)!
 
   const width = parseInt(widthStr, 10) || undefined
   const height = parseInt(heightStr, 10) || undefined
@@ -89,7 +101,7 @@ function extractResize(str: string): Resize {
 }
 
 function extractCrop(str: string): Crop {
-  const [, xStr, yStr, widthStr, heightStr] = str.match(/^(\d*)x(\d*):(\d*)x(\d*)$/)!
+  const [, xStr, yStr, widthStr, heightStr] = str.match(cropPattern)!
 
   const x = parseInt(xStr, 10) || 0
   const y = parseInt(yStr, 10) || 0
